38192e891efd89d91752f231dfe35a5388bc4016
m01344 <88671468+m01344@users.noreply.github.com>
2024-03-03 14:47:47 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vnet_name" {
+  description = "Name of the virtual network"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+}
+
+variable "location" {
+  description = "Azure region where resources will be deployed"
+}
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+}
